802aa2e6e68ca493083e1f54b2ce350311e33a29
AlexRavioli <alexandooop@gmail.com>
2019-07-06 03:27:03 -0400

    Update element-one.component.js
    Testing React

--- src/components/element-one.component.js
+++ src/components/element-one.component.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class ElementOne extends Component {
     render() {
         return (
             <div>
-                <p>Welcome to Element One.</p>
+                <p>Welcome to Chit-Chat.</p>
+                <p>Log in to have access for enhanced features</p>
+            </div>
+            <div>
+                 <button type="submit" id="loginBtn" class="btn btn-lg btn-primary btn-block" value="Submit">Login</button>
+                 <button type="submit" id="loginBtn" class="btn btn-lg btn-primary btn-block" value="Continue Anonymously">Login</button>
             </div>
         )
     }
