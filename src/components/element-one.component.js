c72a662a3286de72838fc05326a8309979f93501
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-07-07 21:56:23 -0400

    Fix compiler error

--- src/components/element-one.component.js
+++ src/components/element-one.component.js
@@ -5,12 +5,14 @@ export default class ElementOne extends Component {
     render() {
         return (
             <div>
-                <p>Welcome to Chit-Chat.</p>
-                <p>Log in to have access for enhanced features</p>
-            </div>
-            <div>
-                 <button type="submit" id="loginBtn" class="btn btn-lg btn-primary btn-block" value="Submit">Login</button>
-                 <button type="submit" id="loginBtn" class="btn btn-lg btn-primary btn-block" value="Continue Anonymously">Login</button>
+                <div>
+                    <p>Welcome to Chit-Chat.</p>
+                    <p>Log in to have access for enhanced features</p>
+                </div>
+                <div>
+                    <button type="submit" id="loginBtn" class="btn btn-lg btn-primary btn-block" value="Submit">Login</button>
+                    <button type="submit" id="loginBtn" class="btn btn-lg btn-primary btn-block" value="Continue Anonymously">Login</button>
+                </div>
             </div>
         )
     }
