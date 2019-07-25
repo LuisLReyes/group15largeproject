9271107131a5581e3917540d204826ca5e43415a
AlexRavioli <alexandooop@gmail.com>
2019-07-25 02:03:35 -0400

    added fetch groups api test

--- src/components/addgroup.component.js
+++ src/components/addgroup.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class ElementThree extends Component {
@@ -12,21 +13,28 @@ export default class ElementThree extends Component {
 		this.changeSearchElement = this.changeSearchElement.bind(this);
 	}
 
+	fetchGroups(){
+		axios.get('https://chit-chat-4331.herokuapp.com/chatroom')
+			.then(res => {
+				console.log(res.data);
+			})
+	}
+
 	changeSearchElement(event) {
 		this.setState({
 			searchElement: event.target.value
-			
+
 		});
 	}
 
     render() {
         return (
             <div>
-                <button type="button"class="btn btn-primary btn-block mb-4" > 
+                <button type="button"className="btn btn-primary btn-block mb-4" onClick={this.fetchGroups} > 
                 	Create New Study Group
                 </button>
 
-                <input type="text" class="form-control" placeholder="Search Group" aria-label="Search" />
+                <input type="text" className="form-control" placeholder="Search Group" aria-label="Search" />
             </div>
         )
     }
